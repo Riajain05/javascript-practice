6d1b9b4160afa91113076199c4329b087a9384e4
Riya Jain <riya05962@gmail.com>
2024-05-29 20:13:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("hello there");
+//alert("learning javascript");
+
+//let heading=document.getElementsByClassName("myclass");
+//console.log(heading);
+
+//let button = document.getElementsById("btn");
+//console.log(button);
+
+//let para = document.getElementsByTagName("p");
+//console.log(para);
+
+let element = document.querySelector("#btn");   
+console.log(element);
+
+let elements = document.querySelectorAll(".myclass");   //return nodelist
+console.log(elements);
+
+//PROPERTIES
+//tagName //innerHTML //innerText //
+
+let value = document.querySelector("div");
+console.log(value);
+
+console.log(value.innerHTML); //return everything with tags
+console.log(value.innerText); //return inside text only
+console.log(value.)
